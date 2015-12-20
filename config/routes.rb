f061efd6e0e842d520bff63c2b7148e17e01d74e
ptibuc <ptibuc@gmail.com>
2015-12-20 14:25:08 +0100

    Ajout d'un controleur Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  #get 'pages/home'
+  #get 'pages/contact'
+
+  get 'home' => 'pages#home'
+  get 'contact' => 'pages#contact'
+
   resources :microposts
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
