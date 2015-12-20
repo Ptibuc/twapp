24074e5af8160807ce7aa2651ec616aaa75ee18e
ptibuc <ptibuc@gmail.com>
2015-12-20 15:03:26 +0100

    MAJ des premières pages statiques

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
 class PagesController < ApplicationController
-  def home
+  def home4
+    @title = "Home"
   end
 
   def contact
+    @title = "Nous contacter"
   end
 
   def about
+    @title = "A propos"
   end
 end
