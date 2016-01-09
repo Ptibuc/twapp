6b795ab7bae3f8f095e342f03095f50968a4d408
ptibuc <ptibuc@gmail.com>
2016-01-09 16:04:42 +0100

    Fin de la première partie de modélisation des utiliseaurs

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,15 @@
 
 class User < ActiveRecord::Base
   has_many :microposts
+
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :nom, presence: { message: "Le nom doit être renseigné" },
+                  length: { :maximum => 50, message: "Le nom ne doit pas dépasser 50 caractères"}
+
+  validates :email, presence: { message: "> L'adresse email doit être renseignée" },
+                    uniqueness: { :case_sensitive => false, message: "> Vous semblez être déjà inscrit !" },
+                    format: { :with => email_regex, message: "> Le format de l'adresse email n'est pas bon" }
+
+  #validates :nom, :presence => true
 end
