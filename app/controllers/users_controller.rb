1623a0594f61e9bfdf8024c8a516e55f6451a613
ptibuc <ptibuc@gmail.com>
2016-01-09 17:09:20 +0100

    creation du mot de passe avant cryptage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -83,6 +83,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:nom, :email)
+      params.require(:user).permit(:nom, :email, :password, :password_confirmation)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,14 +12,43 @@
 class User < ActiveRecord::Base
   has_many :microposts
 
+  attr_accessor :password
+
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  validates :nom, presence: { message: "Le nom doit être renseigné" },
-                  length: { :maximum => 50, message: "Le nom ne doit pas dépasser 50 caractères"}
+  validates :nom, presence: { message: "> Le nom doit être renseigné" },
+                  length: { :maximum => 50, message: "> Le nom ne doit pas dépasser 50 caractères"}
 
   validates :email, presence: { message: "> L'adresse email doit être renseignée" },
                     uniqueness: { :case_sensitive => false, message: "> Vous semblez être déjà inscrit !" },
                     format: { :with => email_regex, message: "> Le format de l'adresse email n'est pas bon" }
 
+  # Crée automatique l'attribut virtuel 'password_confirmation'.
+  validates :password,  :presence     => true,
+                        :confirmation => true,
+                        :length       => { :within => 6..40 }
   #validates :nom, :presence => true
+
+  before_save :encrypt_password
+
+  # Retour true (vrai) si le mot de passe correspond.
+  def has_password?(password_soumis)
+    # Compare encrypted_password avec la version cryptée de
+    # password_soumis.
+  end
+
+  private
+
+    def encrypt_password
+      # self.encrypted_password permet de définir la propriéé dans l'objet utilisable en dehors de la zone privée
+      # sans le self, rien ne serait retourné ou une erreur
+      # on devrait faire pareil dans l'appel à la fonction encrypt
+      # mais le self. est sous-entendu dans ce cas là (l'expérience dira pourquoi)
+      self.encrypted_password = encrypt(password)
+      #self.email = self.nom => à l'enregistrement, l'adresse email prend la valeur du nom. Ca fonctionne, j'ai testé
+    end
+
+    def encrypt(string)
+      string # Implémentation provisoire !
+    end
 end
