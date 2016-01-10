b2cd0586cb6aff2ec40f50f2480b64c57264f270
ptibuc <ptibuc@gmail.com>
2016-01-10 13:59:06 +0100

    Modélisation des utilisateur + fiche utilisateur front

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,10 @@ module ApplicationHelper
     image_tag("google-logo.jpg", :alt => titre, :title => titre, :class => "round")
   end
 
+  def gravatar_for(user, options = { :size => 50})
+    gravatar_image_tag(user.email.downcase,   :alt => user.nom,
+                                              :class => 'gravatar',
+                                              :gravatar => options)
+  end
+
 end
